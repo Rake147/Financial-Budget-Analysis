10ec5d106e3a2ddd542d00f9146ba3b73f78bcc6
Rakesh Kamaraj <102719331+Rake147@users.noreply.github.com>
2022-11-04 11:10:54 +0530

    Add files via upload

---
+++ Financial Budget Analysis.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[3]:
+
+
+data=pd.read_csv('C:/Users/Rakesh/Datasets/India_Budget_2021.csv')
+
+
+# In[4]:
+
+
+data.head()
+
+
+# In[5]:
+
+
+print(data)
+
+
+# In[7]:
+
+
+data=data.dropna()
+
+
+# In[9]:
+
+
+data=data.iloc[[0,8,11,14,18,23,41,42,43],:]
+row={'Department /Ministry': 'OTHERS', 'Fund allotted(in ₹crores)': 592971.0800000001}
+data=data.append(row, ignore_index= True)
+print(data)
+
+
+# In[10]:
+
+
+data.plot.bar(x='Department /Ministry', y='Fund allotted(in ₹crores)')
+
+
+# In[13]:
+
+
+df = data['Fund allotted(in ₹crores)']
+labels = data['Department /Ministry']
+plt.figure(figsize=(7,7))
+plt.pie(df, labels=labels, autopct='%1.1f%%',startangle=90, pctdistance=0.85, shadow = True)
+central_circle = plt.Circle((0,0), 0.5, color='white')
+fig=plt.gcf()
+fig.gca().add_artist(central_circle)
+plt.rc('font', size=12)
+plt.title('Distribution of the Budget', fontsize=20)
+plt.show()
+
